6c1ce40371075b8bc8fbceebc7e7de590adf0500
Victor Hugo <victor.hugo.origins@gmail.com>
2024-01-14 01:22:24 -0300

    small refactoring

--- src/main/kotlin/io/github/victorhsr/hermes/core/DSLGeneratorFactory.kt
+++ src/main/kotlin/io/github/victorhsr/hermes/core/DSLGeneratorFactory.kt
@@ -4,14 +4,11 @@ import io.github.victorhsr.hermes.core.gen.CodeBlockGenerator
 import io.github.victorhsr.hermes.core.gen.DSLGenerator
 import io.github.victorhsr.hermes.core.gen.MethodSpecGenerator
 
-class DSLGeneratorFactory {
+object DSLGeneratorFactory {
 
-    companion object {
-        fun create(): DSLGenerator {
-            val codeBlockGenerator = CodeBlockGenerator()
-            val methodSpecGenerator = MethodSpecGenerator(codeBlockGenerator)
-            return DSLGenerator(methodSpecGenerator)
-        }
+    fun create(): DSLGenerator {
+        val codeBlockGenerator = CodeBlockGenerator()
+        val methodSpecGenerator = MethodSpecGenerator(codeBlockGenerator)
+        return DSLGenerator(methodSpecGenerator)
     }
-
 }
